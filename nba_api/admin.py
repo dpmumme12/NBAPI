317e5f9bc43cafce30d341e0c36452bcee02d9ac
Doug Mumme <dougmumme@gmail.com>
2021-04-16 23:30:32 -0500

    added models to the admin page

--- nba_api/admin.py
+++ nba_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, players, statistics
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(players)
+admin.site.register(statistics)
